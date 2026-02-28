e30e8c8c16fb8dc23e321ab9db0e84e8fdfa96b8
Asdeire <kaoleksandr7@gmail.com>
2026-02-28 12:13:45 +0200

    feat: add server setup with json-server and configure API rewrites

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
+})
+
+server.use(router)
+
+module.exports = server
